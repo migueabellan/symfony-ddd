5d83633c1e33f6ad64b299f0dfeb17c1706603fe
Miguel Angel <migueabellan@gmail.com>
2022-04-25 10:04:49 +0200

    feat: install symfony 6

---
+++ config/bundles.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    Core\Shared\Infrastructure\Symfony\Bundle\DbalTypeRegisterBundle::class => ['all' => true],
+
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+];
